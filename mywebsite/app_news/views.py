41860d9fb98c5cf300a0c5462a66a47af47ab367
mks_dev <maximtimofeev.dev@gmail.com>
2024-03-14 20:46:43 +0300

    update views.py(app_news)

--- mywebsite/app_news/views.py
+++ mywebsite/app_news/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Article
+
+
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, 'app_news/article/list.html', {'articles': articles})
+
+
+def article_detail(request, id):
+    article = get_object_or_404(Article, id=id)
+    return render(request, 'app_news/article/detail.html', {'article': article})
