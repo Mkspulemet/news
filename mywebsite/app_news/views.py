de486d6712018fc66e551d2d3fbc3c20a032d46c
mks_dev <maximtimofeev.dev@gmail.com>
2024-03-26 19:56:54 +0300

    измнил views.py

--- mywebsite/app_news/views.py
+++ mywebsite/app_news/views.py
@@ -4,10 +4,10 @@ from .models import Article
 
 
 def article_list(request):
-    articles = Article.objects.all()
+    articles = Article.published.all()
     return render(request, 'app_news/article/list.html', {'articles': articles})
 
 
 def article_detail(request, id):
-    article = get_object_or_404(Article, id=id)
+    article = get_object_or_404(Article, id=id, status=Article.Status.PUBLISHED)
     return render(request, 'app_news/article/detail.html', {'article': article})
