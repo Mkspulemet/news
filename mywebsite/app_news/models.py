dec90fd0aafa14aec272bb20f05475a25c02eca9
mks_dev <maximtimofeev.dev@gmail.com>
2024-04-09 22:10:05 +0300

    add model Tags

--- mywebsite/app_news/models.py
+++ mywebsite/app_news/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -23,6 +24,7 @@ class Article(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
+    tags = TaggableManager()
 
     status = models.CharField(max_length=2, choices=Status.choices,
                               default=Status.DRAFT)
--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app_news.apps.AppNewsConfig',
+    'taggit',
 ]
 
 MIDDLEWARE = [
