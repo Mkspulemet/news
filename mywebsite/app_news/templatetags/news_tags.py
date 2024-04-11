96d14b02afa43f4a61c8b00e2cd19b269a2453b6
Тимофеев Максим Сергеевич <tsimafeyeu_ms@npc.by>
2024-04-11 15:01:08 +0300

    add markdown

--- mywebsite/app_news/templatetags/news_tags.py
+++ mywebsite/app_news/templatetags/news_tags.py
@@ -1,9 +1,14 @@
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
 from app_news.models import Article
-
+import markdown
 register = template.Library()
 
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
 
 
 @register.simple_tag
